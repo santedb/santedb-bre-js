04bc25e353f8097435b1a46ccc45c04fe2115ce3
Justin Fyfe <justin@fyfesoftware.ca>
2022-09-08 11:14:09 -0400

    Updated license headers

--- SanteDB.BusinessRules.JavaScript/lib/santedb-model.js
+++ SanteDB.BusinessRules.JavaScript/lib/santedb-model.js
@@ -16,7 +16,7 @@
  * the License.
  * 
  * User: fyfej
- * Date: 2021-8-27
+ * Date: 2022-5-30
  */
 
 // SanteDB.Core.Model.NonVersionedEntityData, SanteDB.Core.Model, Version=1.1.0.35405, Culture=neutral, PublicKeyToken=null
