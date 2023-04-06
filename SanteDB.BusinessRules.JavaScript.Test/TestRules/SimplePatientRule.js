02535b47565c56cba89a2d40801fc9f8b1cf5e92
Justin Fyfe <justin@fyfesoftware.ca>
2023-04-06 11:43:33 -0400

    Updated to fix exception thrown being hidden

--- SanteDB.BusinessRules.JavaScript.Test/TestRules/SimplePatientRule.js
+++ SanteDB.BusinessRules.JavaScript.Test/TestRules/SimplePatientRule.js
@@ -30,9 +30,11 @@ SanteDBBre.AddBusinessRule("Patient", "AfterInsert", {}, function (patient) {
     console.assert(simplePatient.genderConceptModel != null, "Gender is null");
     console.assert(simplePatient.genderConceptModel.mnemonic == "Female", "Expected Female");
     console.assert(simplePatient.participation != null, "Participation missing");
+    console.assert(simplePatient.participation.RecordTarget != null, "Record Target missing");
     console.assert(simplePatient.participation.RecordTarget[0] != null, "Record Target missing");
     console.assert(simplePatient.name != null, "Names null");
     console.assert(simplePatient.name.Legal != null, "Names missing Legal");
+    console.assert(Array.isArray(simplePatient.name.Legal), "Names is not array");
     console.assert(simplePatient.name.Legal[0].component != null, "Name missing components");
     console.assert(simplePatient.name.Legal[0].component.Given[0] == "James", "Expected James as given name");
     console.assert(simplePatient.name.Legal[0].component.Family[0] == "Smith", "Expected Smith as family");
